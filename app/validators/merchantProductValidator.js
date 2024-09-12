00cb35bbccf47f62cd305156ff4759757f0052f6
maulik-buzz <maulik.buzz@gmail.com>
2024-09-12 09:53:02 +0530

    add validation in product variant

--- app/validators/merchantProductValidator.js
+++ app/validators/merchantProductValidator.js
@@ -1,4 +1,4 @@
-const { body } = require('express-validator'); 
+const { body } = require('express-validator');
 
 const merchantProductValidation = () => {
   return [
@@ -9,10 +9,15 @@ const merchantProductValidation = () => {
     body('product_id')
       .optional()
       .isInt().withMessage('Product ID must be an integer'),
+    body().custom(body => {
+      if (!body.merchant_id && !body.product_id) {
+        throw new Error('Either Merchant ID or Product ID is required');
+      }
+      return true;
+    }),
   ];
 };
- 
 
 module.exports = {
-  merchantProductValidation 
+  merchantProductValidation
 };
