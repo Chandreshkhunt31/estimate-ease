5110f5b952a654cee8a16dad2552d2ee72723a60
maulik-buzz <maulik.buzz@gmail.com>
2024-09-24 16:06:14 +0530

    create quotation details model

---
+++ app/models/quotationDetails.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const QuotationDetail = sequelize.define('QuotationDetail', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    quot_number: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    quot_by: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    created_by: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+      }
+    },
+    customer_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'customers',
+        key: 'id'
+      }
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    tableName: 'quotation_details',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    underscored: true,
+    paranoid: true
+  });
+
+  QuotationDetail.associate = function (models) {
+
+    QuotationDetail.belongsTo(models.User, {
+      foreignKey: 'created_by',
+      as: 'users'
+    });
+
+    QuotationDetail.belongsTo(models.Customer, {
+      foreignKey: 'customer_id',
+      as: 'customers'
+    });
+  };
+
+  return QuotationDetail;
+};
