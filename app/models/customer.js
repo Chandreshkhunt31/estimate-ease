c256835ff29a6066ef7a302f96b97662ac39b41b
maulik-buzz <maulik.buzz@gmail.com>
2024-09-24 16:05:43 +0530

    create customer model

---
+++ app/models/customer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Customer = sequelize.define('Customer', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    contact_no: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    tableName: 'customers',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    underscored: true,
+    paranoid: true
+  });
+ 
+  return Customer;
+};
+
+
