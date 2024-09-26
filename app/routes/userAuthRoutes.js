8ed24c1447714670ea5a1c8bdb6e802f8704a038
maulikBuzz <maulik.buzz@gmail.com>
2024-09-26 18:31:21 +0530

    remove unnessesory code in user route

--- app/routes/userAuthRoutes.js
+++ app/routes/userAuthRoutes.js
@@ -9,18 +9,7 @@ const userAuthController = require('../controllers/userAuthController');
 router.post('/signup', userSignupValidationRules(), validate, userAuthController.signup);
 router.post('/login', loginValidationRules(), validate, userAuthController.login);
 
-const productController = require('../controllers/productController')
-
-router.get("/product/list", authenticateUserJWT, productController.getProductList);
-router.get("/product/get", authenticateUserJWT, productController.getProduct);
-
-const merchantSubProductController = require('../controllers/merchantSubProductController')
-
-router.post('/merchant-sub-product/add', authenticateUserJWT, merchantSubProductController.addMerchantSubProduct);
-router.put("/merchant-sub-product/edit", authenticateUserJWT, merchantSubProductController.updateMerchantSubProduct);
-router.get("/merchant-sub-product/list", authenticateUserJWT, merchantSubProductController.getMerchantSubProductList);
-router.get("/merchant-sub-product/get", authenticateUserJWT, merchantSubProductController.getMerchantSubProduct);
-router.delete("/merchant-sub-product/delete", authenticateUserJWT, merchantSubProductController.deleteMerchantSubProduct);
+ 
 
 const estimateController = require('../controllers/estimationController')
 
